407c996bc83ad8c86d384506b71580b4f2802a16
pinicilin2006 <pinicilin2006@ya.ru>
2016-04-30 01:15:20 +0500

    Update footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,15 +1,10 @@
-    <footer>
+
         <div class="container">
             <div class="footer-content">
                 <div class="row">
-                    <div class="col-md-12">
-                        <div class="col-md-4">
-                            <div class="copyright-text">
-                                <p>Copyright &copy; 2016  <a href="http://www.tklient.ru">www.tklient.ru</a></p>
-                            </div>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="social-button">
+                    <div class="col-md-12  pagination-centered">
+
+                            <div>
                                 <script type="text/javascript">(function() {
                                   if (window.pluso)if (typeof window.pluso.start == "function") return;
                                   if (window.ifpluso==undefined) { window.ifpluso = 1;
@@ -21,9 +16,8 @@
                                   }})();</script>
                                <div class="pluso" data-background="transparent" data-options="medium,round,line,horizontal,counter,theme=06" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir"></div>
                             </div>
-                        </div>
                     </div>    
                 </div>
             </div>
         </div>
-    </footer>
+
