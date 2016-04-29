3193335315a95732138847f53ca264be4e466d57
pinicilin2006 <pinicilin2006@ya.ru>
2016-04-30 00:55:28 +0500

    fix

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,29 @@
-    <div id="footer">
-      <!-- <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p> -->
-       
-         <p class="text-muted"><a href="http://h-aa.ru">www.h-aa.ru</a></p>
-      
-    </div>
+    <footer>
+        <div class="container">
+            <div class="footer-content">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="col-md-4">
+                            <div class="copyright-text">
+                                <p>Copyright &copy; 2016  <a href="http://h-aa.ru">Хусаинов Артур Амурович</a></p>
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="social-button">
+                                <script type="text/javascript">(function() {
+                                  if (window.pluso)if (typeof window.pluso.start == "function") return;
+                                  if (window.ifpluso==undefined) { window.ifpluso = 1;
+                                    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+                                    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+                                    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+                                    var h=d[g]('body')[0];
+                                    h.appendChild(s);
+                                  }})();</script>
+                               <div class="pluso" data-background="transparent" data-options="medium,round,line,horizontal,counter,theme=06" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir"></div>
+                            </div>
+                        </div>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    </footer>
