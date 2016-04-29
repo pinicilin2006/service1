f731b11b2a7d3b0e9d49ab9f00ccf9c68e1cbcb7
pinicilin2006 <pinicilin2006@ya.ru>
2016-04-30 01:17:11 +0500

    Update footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,23 +1,23 @@
 
-        <div class="container">
-            <div class="footer-content">
-                <div class="row">
-                    <div class="col-md-12  pagination-centered">
+        <!--<div class="container">-->
+        <!--    <div class="footer-content">-->
+        <!--        <div class="row">-->
+        <!--            <div class="col-md-12  pagination-centered">-->
 
-                            <div>
-                                <script type="text/javascript">(function() {
-                                  if (window.pluso)if (typeof window.pluso.start == "function") return;
-                                  if (window.ifpluso==undefined) { window.ifpluso = 1;
-                                    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-                                    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
-                                    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
-                                    var h=d[g]('body')[0];
-                                    h.appendChild(s);
-                                  }})();</script>
-                               <div class="pluso" data-background="transparent" data-options="medium,round,line,horizontal,counter,theme=06" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir"></div>
-                            </div>
-                    </div>    
-                </div>
-            </div>
-        </div>
+        <!--                    <div>-->
+        <!--                        <script type="text/javascript">(function() {-->
+        <!--                          if (window.pluso)if (typeof window.pluso.start == "function") return;-->
+        <!--                          if (window.ifpluso==undefined) { window.ifpluso = 1;-->
+        <!--                            var d = document, s = d.createElement('script'), g = 'getElementsByTagName';-->
+        <!--                            s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;-->
+        <!--                            s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';-->
+        <!--                            var h=d[g]('body')[0];-->
+        <!--                            h.appendChild(s);-->
+        <!--                          }})();</script>-->
+        <!--                       <div class="pluso" data-background="transparent" data-options="medium,round,line,horizontal,counter,theme=06" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir"></div>-->
+        <!--                    </div>-->
+        <!--            </div>    -->
+        <!--        </div>-->
+        <!--    </div>-->
+        <!--</div>-->
 
