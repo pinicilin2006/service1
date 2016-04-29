4ff92ccb8e086f34a7658184adda2d3c998dee48
pinicilin2006 <pinicilin2006@ya.ru>
2016-04-30 00:51:57 +0500

    fix

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,6 @@
     <div id="footer">
       <!-- <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p> -->
-       <div class="container">
+       
          <p class="text-muted"><a href="http://h-aa.ru">www.h-aa.ru</a></p>
-      </div>
+      
     </div>
