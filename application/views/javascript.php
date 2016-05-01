419289ea74102ad16624bf152aebb50cf32fea63
pinicilin2006 <pinicilin2006@ya.ru>
2016-05-01 13:22:31 +0500

    Update javascript.php

--- application/views/javascript.php
+++ application/views/javascript.php
@@ -8,11 +8,4 @@
 	var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 	</script>
 	<!-- {/literal} END JIVOSITE CODE -->
-	<script type='text/javascript'>
-		$(document).keydown(function(e) {
-		    var elid = $(document.activeElement).hasClass('textInput');
-		    if (e.keyCode === 8 && !elid) {
-		        return false;
-		    };
-		});
-	</script>
+
