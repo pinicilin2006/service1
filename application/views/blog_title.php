b4a5ee9a31273b81718cfbe7f8047e2ce0adcc6f
Артур <admin@dinamikasveta.com>
2016-05-15 14:19:25 +0500

    Поменял верхнюю часть страниц

--- application/views/blog_title.php
+++ application/views/blog_title.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div class="row">
 	    <div class="col-sm-9">
-			<div class="blog-header">
-			    <h1 class="blog-title"><a href="http://tklient.ru">TKlient.ru</a> - регулярно обновляемая база заявок <br>по поиску контрактных запчастей</h1>
+<!-- 			<div class="blog-header">
+			    <h1 class="blog-title"><a href="http://tklient.ru">TKlient.ru</a> - </h1>
 			    <p class="lead blog-description">Гарантия низкой цены на контрактные запчасти</p>
 			    <script type="text/javascript">(function() {
 					  if (window.pluso)if (typeof window.pluso.start == "function") return;
@@ -16,6 +16,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					    h.appendChild(s);
 					  }})();</script>
 					<div class="pluso" data-background="#ebebeb" data-options="small,square,line,horizontal,counter,theme=06" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir,email,print"></div>    
+			</div> -->
+			<div class="blog-header">
+					<div class="col-sm-4">
+						<a href="/"><img src="/assets/images/logo.png" alt="..." class="img-thumbnail"></a>
+					</div>
+					<div class="col-sm-8">
+						<h1 class="blog-title">Регулярно обновляемая база заявок по поиску контрактных запчастей</h1>
+						<p class="lead blog-description">Гарантия низкой цены на контрактные запчасти</p>
+						<script type="text/javascript">(function() {
+					  if (window.pluso)if (typeof window.pluso.start == "function") return;
+					  if (window.ifpluso==undefined) { window.ifpluso = 1;
+					    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+					    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+					    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+					    var h=d[g]('body')[0];
+					    h.appendChild(s);
+					  }})();</script>
+					<div class="pluso" data-background="#ebebeb" data-options="small,square,line,horizontal,counter,theme=06" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir,email,print"></div>   						
+					</div>				
 			</div>
 		</div>
 	<div class="col-sm-3">
