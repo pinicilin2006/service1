38f501fff5b8dc0b887d50a52351a9caab359b20
Артур <admin@dinamikasveta.com>
2016-05-19 12:56:52 +0500

    Поправил футер

--- application/views/footer.php
+++ application/views/footer.php
@@ -26,18 +26,23 @@
     <div class="blog-footer">
         <div class='container'>
             <div class='row'>
-                    <div class="col-sm-6 text-right">
+                    <div class="col-sm-4">
                         <ul class="list-inline">
-                            <li><a href="mailto:info@tklient.ru">info@tklient.ru</a></li>
+                            <li><a href="http://h-aa.ru"><small>Изготовление сайта www.h-aa.ru</small></a></li>
+                        </ul>                        
+                    </div>            
+                    <div class="col-sm-2 text-right">
+                        <ul class="list-inline">
+                            <li><a href="mailto:info@tklient.ru"><small>Связаться с нами</small></a></li>
                         </ul>                        
                     </div>
                     <div class="col-sm-6 text-right">
                         <ul class="list-inline">
                         <?php if($this->ion_auth->logged_in()):?>                       
-                            <li><a href="/auth/edit_user/<?=$this->ion_auth->user()->row()->id?>">Личный кабинет</a></li>
+                            <li><a href="/auth/edit_user/<?=$this->ion_auth->user()->row()->id?>"><small>Личный кабинет</small></a></li>
                         <?php endif;?>    
                         <?php if($this->ion_auth->is_admin()):?>
-                            <li><a href="/auth/">Админка</a></li>
+                            <li><a href="/auth/"><small>Админка</small></a></li>
                         <?php endif;?> 
                     </ul>                                              
                     </div>            
