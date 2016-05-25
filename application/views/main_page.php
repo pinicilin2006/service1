ba746932ee3a7ad3421cde5e0b30c1607be758ef
Артур <admin@dinamikasveta.com>
2016-05-25 10:37:06 +0500

    Вернул оператору право удалять объяву

--- application/views/main_page.php
+++ application/views/main_page.php
@@ -154,7 +154,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                       <td data-label="Наименование:"><?=$row['detail_name']?></td>
                       <td data-label="Срочность:"><?=$row['name']?></td>
                       <td data-label="Данные клиента:"><a  href="#info_modal" role="button" id="<?=$row['id_request']?>" class="btn  btn-success" data-toggle="modal">info</a></td>
-                    <?php if($this->ion_auth->in_group('admin')): ?>
+                    <?php if($this->ion_auth->in_group('admin') ||  $this->ion_auth->in_group('operator')): ?>
                       <td data-label="Удалить:"><a  href="/main/request_del/<?=$row['id_request']?>" role="button"class="btn  btn-danger" data-toggle="modal">del</a></td>                      
                     <?php endif; ?>
                     </tr>
