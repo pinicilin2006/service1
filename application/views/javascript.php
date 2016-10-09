a9a73422517dced899fe28bdbaddc966daf640f6
Артур <admin@dinamikasveta.com>
2016-10-09 11:45:44 +0500

    Отключил чат на сайте

--- application/views/javascript.php
+++ application/views/javascript.php
@@ -7,11 +7,11 @@
 	<script type="text/javascript" src="/assets/js/i18n/defaults-ru_RU.js?t=<?php echo(microtime(true)) ?>"></script>			
 	<script type="text/javascript" src="/assets/js/function.js?t=<?php echo(microtime(true)) ?>"></script>
 	<!-- BEGIN JIVOSITE CODE {literal} -->
-	<script type='text/javascript'>
+<!-- 	<script type='text/javascript'>
 	(function(){ var widget_id = '1XDVwtSYcV';var d=document;var w=window;function l(){
 	var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 	</script>
-	<!-- {/literal} END JIVOSITE CODE -->
+ -->	<!-- {/literal} END JIVOSITE CODE -->
 	<!-- Появление кнопки вверх и прокрутка  вверх при нажатие на неё-->
 	<script type="text/javascript">
 	var top_show = 150; // В каком положении полосы прокрутки начинать показ кнопки "Наверх"
