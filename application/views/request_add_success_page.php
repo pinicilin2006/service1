92c628a868abd07b0fbb6f4a646f65614f68beb8
pinicilin2006 <pinicilin2006@ya.ru>
2016-10-10 13:53:57 +0500

    добавил код отслеживания конверсии для гугла

--- application/views/request_add_success_page.php
+++ application/views/request_add_success_page.php
@@ -31,6 +31,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       </div><!-- /.row -->
 
     </div><!-- /.container -->
+	<!-- Google Code for &#1044;&#1086;&#1073;&#1072;&#1074;&#1083;&#1077;&#1085;&#1080;&#1077; &#1079;&#1072;&#1103;&#1074;&#1082;&#1080; Conversion Page -->
+	<script type="text/javascript">
+	/* <![CDATA[ */
+	var google_conversion_id = 1025519354;
+	var google_conversion_language = "en";
+	var google_conversion_format = "3";
+	var google_conversion_color = "ffffff";
+	var google_conversion_label = "0FNzCOj71GoQ-t2A6QM";
+	var google_remarketing_only = false;
+	/* ]]> */
+	</script>
+	<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+	</script>
+	<noscript>
+	<div style="display:inline;">
+	<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/1025519354/?label=0FNzCOj71GoQ-t2A6QM&amp;guid=ON&amp;script=0"/>
+	</div>
+	</noscript>
 
     <?php include_once('footer.php'); ?>
     <?php include_once('javascript.php'); ?>
